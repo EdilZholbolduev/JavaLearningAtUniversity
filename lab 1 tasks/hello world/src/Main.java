de83589d732bbc99e5d4a3c9b21045728b685b47
Edil Zholbolduev <tashtan841@gmail.com>
2023-11-04 16:31:29 +0200

    decide to change content

--- lab 1 tasks/hello world/src/Main.java
+++ lab 1 tasks/hello world/src/Main.java
@@ -2,6 +2,6 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello wonderful Java programming language!");
+        System.out.println("I like coding in java");
     }
 }
