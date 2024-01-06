4027aee42991310ea4442c095132a1e0573a0639
Edil Zholbolduev <tashtan841@gmail.com>
2024-01-06 13:57:11 +0200

    first spring java

---
+++ src/main/java/poland/edu/vistula/frstspringjava/FrstSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package poland.edu.vistula.frstspringjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrstSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrstSpringJavaApplication.class, args);
+	}
+
+}
